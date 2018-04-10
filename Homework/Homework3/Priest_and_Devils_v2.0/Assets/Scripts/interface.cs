bf926eb2404589cb8001703df5174a5d9cef7a6e
zys980808 <zys980808@126.com>
2018-04-10 23:01:07 +0800

    Add files via upload

---
+++ Homework/Homework3/Priest_and_Devils_v2.0/Assets/Scripts/interface.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface SceneController
+{
+    void LoadResources();
+    void UnloadResources();//留空
+}
+
+public interface View_Contrler_Interface_Scene1  //View和Scene1交互的接口（本游戏只有一个场景-_-）
+{
+    void BoatClicked();
+    void CharacterClicked(GameObject character);    //按引用传递，交由SceneController_1 来决定“是哪个”
+    void ClickRestart();     //点击重新开始，别忘了设置GameState为0
+    int GetGameState();      //接口，检测游戏状态；0-->正在游戏; 1-->赢; 2-->输
+}
+
