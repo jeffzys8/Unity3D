b3f44f75849483ec41a4ce631d8b4fb0be408372
jeff <zys980808@126.com>
2018-07-04 23:55:48 +0800

    Homework 8

---
+++ Homework/Homework8/UI/Assets/IMGUI.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IMGUI : MonoBehaviour
+{
+    public Slider healthSlider;
+    public float health = 0.0f;
+    private float resulthealth = 0.0f;
+
+    private Rect HealthBar;
+    private Rect HealthUp;
+    private Rect HealthDown;
+
+
+    void OnGUI()
+    {   
+       
+        HealthBar = new Rect(50, 50, 200, 20);
+        HealthUp = new Rect(105, 80, 40, 20);
+        HealthDown = new Rect(155, 80, 40, 20);
+
+
+        if (GUI.Button(HealthUp, "+"))
+        {
+            resulthealth = resulthealth + 0.1f > 1.0f ? 1.0f : resulthealth + 0.1f;
+        }
+        if (GUI.Button(HealthDown, "-"))
+        {
+            resulthealth = resulthealth - 0.1f < 0.0f ? 0.0f : resulthealth - 0.1f;
+        }
+        
+        health = Mathf.Lerp(health, resulthealth, 0.01f);
+        GUI.HorizontalScrollbar(HealthBar, 0.0f, health, 0.0f, 1.0f);
+        healthSlider.value = health;
+    }
+}
