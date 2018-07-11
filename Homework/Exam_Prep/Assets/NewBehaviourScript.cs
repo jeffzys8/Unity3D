3d88128a28fc709b6d2c00c7464f427f6ea8e6ae
jeff <zys980808@126.com>
2018-07-11 10:46:47 +0800

    文件错位修复

---
+++ Homework/Exam_Prep/Assets/NewBehaviourScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    private UnityEngine.AI.NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                agent.SetDestination(hit.point);
+        }
+	}
+}
